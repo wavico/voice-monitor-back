329b21c04953ecb3dd0358cd2ab4c64103713048
seonmin8284 <seonmin8284@naver.com>
2025-06-02 00:11:20 +0900

    modified:   entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -ex
 
 # Add Prometheus and Grafana to PATH
 export PATH="/usr/local/bin:$PATH"
@@ -26,11 +26,18 @@ wait_for_service localhost 9090 "Prometheus"
 
 # Start Grafana in the background with debug mode
 echo "Starting Grafana..."
-su grafana -c "/usr/sbin/grafana-server \
+/usr/sbin/grafana-server \
     --config=/app/grafana/grafana.ini \
     --homepath=/usr/share/grafana \
     --pidfile=/var/run/grafana/grafana-server.pid \
-    -debug" &
+    --packaging=docker \
+    cfg:default.log.mode=console \
+    cfg:default.paths.logs=/var/log/grafana \
+    cfg:default.paths.data=/var/lib/grafana \
+    cfg:default.paths.plugins=/var/lib/grafana/plugins \
+    cfg:default.paths.provisioning=/etc/grafana/provisioning \
+    --debug &
+
 
 # Wait for Grafana to be ready
 wait_for_service localhost 3000 "Grafana"
