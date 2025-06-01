99fa0a2cffbcb40d2397dc0039bfae94282085c4
seonmin8284 <seonmin8284@naver.com>
2025-06-02 00:52:41 +0900

    modified:   entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,4 +37,4 @@ wait_for_service localhost 3000 "Grafana"
 
 # Start FastAPI
 echo "Starting FastAPI application..."
-uvicorn main:app --host 0.0.0.0 --port 8000 --reload 
+uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000}
